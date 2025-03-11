639620f15941d65bf054cefb893da899156dfc81
Lucas Kimber <lucas.kimber@outlook.com>
2025-03-11 22:50:06 +0000

    Ammended neo-tree key reveal command.

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -13,7 +13,7 @@ return {
                 hijack_netrw = true,
             }
         })
-        vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>', {})
+        vim.keymap.set('n', '<C-n>', ':Neotree filesystem toggle left<CR>', {})
     end
 }
 
